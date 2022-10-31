f6ee10ec638fe4e1c7f487f2b2ad5fb6a9994ccd
Your Name <phamtienquan2001@gmail.com>
2022-10-31 15:19:20 +0700

    .

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
 import MainLayout from "./layouts/MainLayout";
+import React from "react";
 
-const App=()=> {
+const App = () => {
 
   return (
-    <MainLayout/>
+    <>
+      <head>
+        <meta name="google-site-verification" content="m3OErAMdPxtr2LNsB4J0LmfOxkpsKmYMnEUWfruJrwM"/>
+      </head>
+      <body>
+      <MainLayout/>
+      </body>
+    </>
   );
 }
 
