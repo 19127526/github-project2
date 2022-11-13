6ee5d396c38217bb20053b6d39162a3c2f692c6b
Your Name <phamtienquan2001@gmail.com>
2022-11-13 17:02:05 +0700

    .

--- src/pages/testCarousel/Carousel.jsx
+++ src/pages/testCarousel/Carousel.jsx
@@ -1,9 +1,9 @@
 import image from "../../assets/62dfdb668b98a.jpg"
 import {Carousel} from "react-responsive-carousel";
-
+import "./Carousel.css"
 const Carousel2 = () => {
   return (
-    <div className="carousel-wrapper">
+    <div className="main-slide">
       <Carousel showArrows={true} infiniteLoop useKeyboardArrows autoPlay >
         <div>
           <img src={image}/>
